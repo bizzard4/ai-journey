a2bfeb767070f84a484cd6c64daa054386466724
Francois Gingras2 <francois.gingras89@gmail.com>
2017-07-19 21:14:59 -0400

    Day 1, basic RNN from online tutorial in Python

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ai-journey
-My journey self-learning AI and machine learning
+# My AI journey
+Follow me toward my journey learning AI, machine learning, data analysis, etc.
+
+## Day 1 - RNN
+I am a new traveler. I am scared, the world is so big and I am so small. But, I want to see it. I decided to visit a well known and secure place named Recurrent Neural Network.
---
+++ d1-rnn/rnn.py
@@ -0,0 +1,151 @@
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+
+# This is a simple implementation of a plain Recurrent Neural Netowrk
+
+# This fun day was done with 
+# http://www.wildml.com/2015/09/implementing-a-neural-network-from-scratch/
+
+print("Day 1 - RNN")
+
+# Dataset generation, generate 2 overlapping sets of point.
+np.random.seed(0)
+X, y = sklearn.datasets.make_moons(1000, noise=0.2)
+plt.scatter(X[:,0], X[:,1], s=30, c=y, cmap=plt.cm.Spectral)
+
+# %% 4 
+# Helper function to plot a decision boundary. 
+# If you don't fully understand this function don't worry, it just generates the contour plot below. 
+def plot_decision_boundary(pred_func): 
+    # Set min and max values and give it some padding 
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5 
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5 
+    h = 0.01 
+    # Generate a grid of points with distance h between them 
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h)) 
+    # Predict the function value for the whole gid 
+    Z = pred_func(np.c_[xx.ravel(), yy.ravel()]) 
+    Z = Z.reshape(xx.shape) 
+    # Plot the contour and training examples 
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral) 
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral) 	
+
+# We do a logistic regression to show it cant separate this dataset properly.
+#clf = sklearn.linear_model.LogisticRegressionCV()
+#clf.fit(X, y)
+#plot_decision_boundary(lambda x: clf.predict(x))
+#plt.title("Logistic Regression")
+#plt.show()
+
+# Model:
+# 3 layers, input, hidden, output
+# Hidden layer node count : vary
+# Activation function : tanh
+# Output : softmax (0 or 1 as probability)
+# Error loss function : cross-entropy loss
+# To find minimum : gradient descent with backpropagation algorithm
+
+num_examples = len(X)
+nn_input_dim = 2
+nn_output_dim = 2
+
+# Gradiant descent parameters
+epsilon = 0.01
+reg_lambda = 0.01
+
+# The loss function, cross-entropy loss
+# Helper function to evaluate the total loss on the dataset
+def calculate_loss(model):
+    W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
+    # Forward propagation to calculate our predictions
+    z1 = X.dot(W1) + b1
+    a1 = np.tanh(z1)
+    z2 = a1.dot(W2) + b2
+    exp_scores = np.exp(z2)
+    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+    # Calculating the loss
+    corect_logprobs = -np.log(probs[range(num_examples), y])
+    data_loss = np.sum(corect_logprobs)
+    # Add regulatization term to loss (optional)
+    data_loss += reg_lambda/2 * (np.sum(np.square(W1)) + np.sum(np.square(W2)))
+    return 1./num_examples * data_loss
+
+# Helper function to predict an output (0 or 1)
+def predict(model, x):
+    W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
+    # Forward propagation
+    z1 = x.dot(W1) + b1
+    a1 = np.tanh(z1)
+    z2 = a1.dot(W2) + b2
+    exp_scores = np.exp(z2)
+    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+    return np.argmax(probs, axis=1)
+
+# This function learns parameters for the neural network and returns the model.
+# - nn_hdim: Number of nodes in the hidden layer
+# - num_passes: Number of passes through the training data for gradient descent
+# - print_loss: If True, print the loss every 1000 iterations
+def build_model(nn_hdim, num_passes=20000, print_loss=False):
+     
+    # Initialize the parameters to random values. We need to learn these.
+    np.random.seed(0)
+    W1 = np.random.randn(nn_input_dim, nn_hdim) / np.sqrt(nn_input_dim)
+    b1 = np.zeros((1, nn_hdim))
+    W2 = np.random.randn(nn_hdim, nn_output_dim) / np.sqrt(nn_hdim)
+    b2 = np.zeros((1, nn_output_dim))
+ 
+    # This is what we return at the end
+    model = {}
+     
+    # Gradient descent. For each batch...
+    for i in range(0, num_passes):
+ 
+        # Forward propagation
+        z1 = X.dot(W1) + b1
+        a1 = np.tanh(z1)
+        z2 = a1.dot(W2) + b2
+        exp_scores = np.exp(z2)
+        probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+ 
+        # Backpropagation
+        delta3 = probs
+        delta3[range(num_examples), y] -= 1
+        dW2 = (a1.T).dot(delta3)
+        db2 = np.sum(delta3, axis=0, keepdims=True)
+        delta2 = delta3.dot(W2.T) * (1 - np.power(a1, 2))
+        dW1 = np.dot(X.T, delta2)
+        db1 = np.sum(delta2, axis=0)
+ 
+        # Add regularization terms (b1 and b2 don't have regularization terms)
+        dW2 += reg_lambda * W2
+        dW1 += reg_lambda * W1
+ 
+        # Gradient descent parameter update
+        W1 += -epsilon * dW1
+        b1 += -epsilon * db1
+        W2 += -epsilon * dW2
+        b2 += -epsilon * db2
+         
+        # Assign new parameters to the model
+        model = { 'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
+         
+        # Optionally print the loss.
+        # This is expensive because it uses the whole dataset, so we don't want to do it too often.
+        if print_loss and i % 1000 == 0:
+          print("Loss after iteration %i: %f" %(i, calculate_loss(model)))
+     
+    return model
+
+# Now we can play
+hidden_node_count = 10
+model = build_model(hidden_node_count, print_loss=True)
+
+plot_decision_boundary(lambda x: predict(model, x))
+plt.title("Decision Boundary for hidden layer size " + str(hidden_node_count))
+plt.show()
+
+print("Day 1 is over")
